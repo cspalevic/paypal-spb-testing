86997da65bf53535f1841894e030c5a4b9ea3180
Cedomir Spalevic <cedomir.spalevic@gmail.com>
2023-06-13 18:08:11 -0500

    Adding shipping and pickup options

--- src/utils/paypal.js
+++ src/utils/paypal.js
@@ -87,9 +87,67 @@ export const createOrder = (selector, {
                                                 currency_code: "USD",
                                             }
                                         }
-                                    ]
+                                    ],
+                                    shipping: {
+                                        name: {
+                                            full_name: "Charlie Spalevic"
+                                        },
+                                        email_address: "hey@cspalevc.com",
+                                        options: [
+                                            {
+                                                id: "shipping-1",
+                                                label: "Express Shipping",
+                                                type: "SHIPPING",
+                                                amount: {
+                                                    value: "10",
+                                                    currency_code: "USD"
+                                                },
+                                                selected: true
+                                            },
+                                            {
+                                                id: "shipping-2",
+                                                label: "Super Express Shipping",
+                                                type: "SHIPPING",
+                                                amount: {
+                                                    value: "20",
+                                                    currency_code: "USD"
+                                                },
+                                                selected: false
+                                            },
+                                            {
+                                                id: "pickup-1",
+                                                label: "Pickup in Store",
+                                                type: "PICKUP",
+                                                amount: {
+                                                    value: "10",
+                                                    currency_code: "USD"
+                                                },
+                                                selected: false
+                                            },
+                                            {
+                                                id: "pickup-2",
+                                                label: "Drop off to car",
+                                                type: "PICKUP",
+                                                amount: {
+                                                    value: "20",
+                                                    currency_code: "USD"
+                                                },
+                                                selected: false
+                                            }
+                                        ],
+                                        address: {
+                                            address_line_1: "310 W. Chicago Ave",
+                                            admin_area_2: "Westmont",
+                                            admin_area_1: "IL",
+                                            postal_code: "60559",
+                                            country_code: "US"
+                                        }
+                                    }
                                 }
-                            ]
+                            ],
+                            application_context: {
+                                shipping_preference: "GET_FROM_FILE"
+                            }
                         })
                     
                         console.log(response);
